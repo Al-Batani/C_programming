e40b93927133803db61c6061c3a521eac66f11dc
Al-Batani <dmbatani@gmail.com>
2021-06-28 03:36:51 -0400

    adding one.c

---
+++ one.c
@@ -0,0 +1,27 @@
+
+
+   #include<stdio.h>
+
+
+     int main(){
+
+
+       int number1;
+       int number2;
+       int sum;
+
+       printf("Enter first number:");
+       scanf("%d",&number1);
+
+
+       printf("Enter second number:");
+       scanf("%d", &number2);
+       sum= number1 + number2;
+
+
+       printf("The sum is:  %d\n ",sum);
+
+        
+       return 0;
+		    
+        }
